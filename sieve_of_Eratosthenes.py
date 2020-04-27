e3e771d687d0558a151d307fedf6676a74af5aae
Frankenstein <44138096+konvergentk@users.noreply.github.com>
2020-04-27 21:07:23 +0300

    Create sieve_of_Eratosthenes.py

---
+++ sieve_of_Eratosthenes.py
@@ -0,0 +1,22 @@
+n = int(input("вывод простых чисел до"))
+a = [0] * n
+b = []
+
+for i in range(2, n):
+    a[i] = i
+ 
+m = 2
+while m < n:
+    if a[m] != 0:
+        j = m * 2
+        while j < n:
+            a[j] = 0
+            j = j + m
+    m += 1
+ 
+for i in a:
+    if a[i] != 0:
+        b.append(a[i])
+ 
+del a
+print(b)
